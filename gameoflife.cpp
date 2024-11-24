9b749ce07d8363216e4eb50b8a54e1cbf044b09e
ThatChillProgrammer <bite74034@gmail.com>
2024-11-23 23:39:23 -0800

    basic

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
-
+class gameoflife{
+    int x = 10, y = 10;
+    int x_pos, y_pos;
+    gameoflife(int xp, int yp){
+        x_pos = xp;
+        y_pos = yp;
+    }
+    void testMethod(){
+        std::cout << "Hello World!" << std::endl;
+    }
+    
+};
 
 
 int main(){
