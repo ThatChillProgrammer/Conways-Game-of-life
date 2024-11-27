2dc8803ce4b26061e50e29bb3f41503bfc6bac7f
ThatChillProgrammer <bite74034@gmail.com>
2024-11-27 13:28:43 -0800

    8

--- grid.cpp
+++ grid.cpp
@@ -41,7 +41,7 @@ void Grid::CgolRules(int row, int cols){
     if(cellStatus==0 and cellNeighbor==3){
         grid[row][cols][0] = 1;
     }
-    grid[row][cols][1] = 0;
+    
 }
 
 void Grid::Borders(int row, int cols){
@@ -50,7 +50,9 @@ void Grid::Borders(int row, int cols){
             for(int i=row;i<row+2;i++){
                 for(int j=cols;j<cols+2;j++){
                     if(i!=row or j!=cols){ 
-                    grid[row][cols][1] = grid[row][cols][1] + 1;
+                    if(grid[i][j][0]==1){
+                        grid[row][cols][1] = grid[row][cols][1] + 1;
+                    }
                     }
                 }
             }
@@ -59,7 +61,9 @@ void Grid::Borders(int row, int cols){
             for(int i=row;i<row+2;i++){
                 for(int j=cols-1;j<cols+1;j++){
                     if(i!=row or j!=cols){
-                        grid[row][cols][1]++;
+                        if(grid[i][j][0]==1){
+                        grid[row][cols][1] = grid[row][cols][1] + 1;
+                    }
                     }
                 }
             }
@@ -68,7 +72,9 @@ void Grid::Borders(int row, int cols){
             for(int i=row-1;i<row+1;i++){
                 for(int j=cols;j<cols+2;j++){
                     if(i!=row or j!=cols){
-                        grid[row][cols][1]++;
+                       if(grid[i][j][0]==1){
+                        grid[row][cols][1] = grid[row][cols][1] + 1;
+                    }
                     }
                 }
             }
@@ -77,7 +83,9 @@ void Grid::Borders(int row, int cols){
             for(int i=row-1;i<row;i++){
                 for(int j=cols-1;j<cols+2;j++){
                     if(i!=row or j!=cols){
-                        grid[row][cols][1]++;
+                         if(grid[i][j][0]==1){
+                        grid[row][cols][1] = grid[row][cols][1] + 1;
+                    }
                     }
                 }
             }
@@ -87,7 +95,9 @@ void Grid::Borders(int row, int cols){
             for(int i=row;i<row+2;i++){
                 for(int j=cols-1;j<cols+2;j++){
                     if(i!=row or j!=cols){
-                        grid[row][cols][1]++;
+                    if(grid[i][j][0]==1){
+                        grid[row][cols][1] = grid[row][cols][1] + 1;
+                    }
                     }
                 }
             }
@@ -96,7 +106,9 @@ void Grid::Borders(int row, int cols){
             for(int i=row-1;i<row+2;i++){
                 for(int j=cols;j<cols+2;j++){
                     if(i!=row or j!=cols){
-                       grid[row][cols][1]++;
+                       if(grid[i][j][0]==1){
+                        grid[row][cols][1] = grid[row][cols][1] + 1;
+                    }
                     }
                 }
             }
@@ -105,7 +117,9 @@ void Grid::Borders(int row, int cols){
             for(int i=row-1;i<row+1;i++){
                 for(int j=cols-1;j<cols+2;j++){
                     if(i!=row or j!=cols){
-                        grid[row][cols][1]++;
+                        if(grid[i][j][0]==1){
+                        grid[row][cols][1] = grid[row][cols][1] + 1;
+                    }
                     }
                     
                 }
@@ -115,7 +129,9 @@ void Grid::Borders(int row, int cols){
             for(int i=row-1;i<row+2;i++){
                 for(int j=cols-1;j<cols+1;j++){
                     if(i!=row or j!=cols){
-                        grid[row][cols][1]++;
+                        if(grid[i][j][0]==1){
+                        grid[row][cols][1] = grid[row][cols][1] + 1;
+                    }
                     }
                     
                 }
@@ -129,31 +145,34 @@ void Grid::Borders(int row, int cols){
 void Grid::StatusFunction(int row, int cols){
    if((row!=19 and row!=0) and (cols!=19 and cols!=0)){
         for(int i=row-1; i<row+2;i++){
-            for(int j=cols-1;i<cols+2;i++){
-                if(i!=row or i!=cols){
+            for(int j=cols-1;j<cols+2;j++){
+                if(i!=row or j!=cols){
                      if(grid[i][j][0]==1){
-                        grid[row][cols][1]++;
+                        grid[row][cols][1] = grid[row][cols][1] + 1;
                     }
                 }
             }
         }
     } else {
-        std::cout << "check";
         Borders(row,cols);
-    }
+    }   
 };
 
 void Grid::Draw(){
      for(int row=0;row<numRows;row++){
         for(int column=0; column<numCols;column++){
             StatusFunction(row, column);
+        }
+    }
+     for(int row=0;row<numRows;row++){
+        for(int column=0; column<numCols;column++){
             if(grid[row][column][0]==0){    
                 DrawRectangle(column*cellsize+1,row*cellsize+1,cellsize-1,cellsize-1,DARKGRAY);
             } else {
                 DrawRectangle(column*cellsize+1,row*cellsize+1,cellsize-1,cellsize-1,YELLOW);
             }
             CgolRules(row,column);
-            std::cout << grid[1][0][0];
+            grid[row][column][1] = 0;
         }
     }
 }
--- main.cpp
+++ main.cpp
@@ -3,23 +3,26 @@
 
 int main() {
 
-    InitWindow(700,700,"RayLib Wind");
+    InitWindow(600,600,"Connoway's Game Of Life");
 
-    SetTargetFPS(1);
+    SetTargetFPS(25);
 
     Grid grid = Grid();
     grid.Initialize();
-    grid.grid[0][0][0] = 1;
-    grid.grid[1][0][0] = 1;
-    grid.grid[0][1][0] = 1;
-
-    grid.grid[10][19][0]= 1;
 
     grid.grid[7][7][0] = 1;
-    grid.grid[7][8][0] = 1;
     grid.grid[8][7][0] = 1;
+    grid.grid[7][8][0] = 1;
+    grid.grid[6][7][0] = 1;
+
+    grid.grid[7][5][0] = 1;
+    grid.grid[7][7][0] = 1;
+    grid.grid[7][7][0] = 1;
+    grid.grid[7][7][0] = 1;
+    grid.grid[7][7][0] = 1;
 
     grid.Print();
+    
     while(!WindowShouldClose()){
         BeginDrawing();
 
